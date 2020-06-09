670d4886325d49ca02de7a67180bb58ac3ca64eb
vagrant <youyo1204@gmail.com>
2020-06-09 04:27:07 +0000

    kadai2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,23 +4,39 @@ class BooksController < ApplicationController
   end
   
   def show
+    @book = Book.find(params[:id])
   end
 
   
   
   def index 
+    @books = Book.all
     @book = Book.new
   end
    
   def create
-    book = Book.new(book_params)
-    Book.save
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+    
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update 
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
     redirect_to books_path
   end
 
-  def edit
-  end
-  
   private
   def book_params
     params.require(:book).permit(:title, :body)
