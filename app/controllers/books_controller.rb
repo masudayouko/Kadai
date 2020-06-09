3721cba8158ca6945e33c4098ec57d14a67fc553
vagrant <youyo1204@gmail.com>
2020-06-09 10:18:16 +0000

    kadai

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,15 +10,19 @@ class BooksController < ApplicationController
   
   
   def index 
-    @books = Book.all
+    @books= Book.all
     @book = Book.new
   end
    
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+    else #def indexの代わり
+      @books= Book.all
+      render action: :index
   end
+end
     
 
   def edit
@@ -26,15 +30,18 @@ class BooksController < ApplicationController
   end
   
   def update 
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       redirect_to book_path(@book), notice: 'Book was successfully updated.'
+    else #def editの代わり
+      render action: :edit
+    end
   end
 
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path, notice: 'Book was successfully destroyed.'
   end
 
   private
